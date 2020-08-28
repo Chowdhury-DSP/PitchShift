f85641bb79f4bbc4116bf2ef6d601fcebdc0f879
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2020-08-28 00:00:10 -0700

    Fix sed command for osx

--- setup.sh
+++ setup.sh
@@ -8,7 +8,14 @@ mv src/TempPlugin.h src/${plugin_name}.h
 mv src/TempPlugin.cpp src/${plugin_name}.cpp
 
 echo "Setting up source files..."
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed_cmd="sed -i \'\' -e"
+else
+    sed_cmd="sed -i"
+fi
+
 declare -a source_files=("validate.sh" "win_builds.sh" "CMakeLists.txt" "src/CMakeLists.txt" "src/${plugin_name}.h" "src/${plugin_name}.cpp")
 for file in "${source_files[@]}"; do
-    sed -i "s/TempPlugin/${plugin_name}/g" $file
+    $sed_cmd "s/TempPlugin/${plugin_name}/g" $file
 done
