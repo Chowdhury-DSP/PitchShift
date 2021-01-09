45fb2f542b43cf0b9e36b001f5e54564a29c0ced
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2021-01-09 15:23:19 -0800

    Switch master to main

--- setup.sh
+++ setup.sh
@@ -47,7 +47,7 @@ clean_git_history(){
     git checkout --orphan new-branch
     git add -A
     git commit -m "Initial commit"
-    git branch -D master
+    git branch -D main
     git branch -m main   
 }
 
